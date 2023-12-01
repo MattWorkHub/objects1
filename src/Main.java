27ba3b0087d1178e57d32134f25e40cb9d88de6e
MattWorkHub <150373956+MattWorkHub@users.noreply.github.com>
2023-12-01 15:47:24 +0100

    test

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String args[]) {
+        Student student = new Student(0,"Giovanni","Giorgio");
+        Student student1 = new Student(1,"Piero","Piers");
+
+        System.out.println(student + "\r\n" + student1);
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,39 @@
+public class Student {
+    public int id;
+    public String name;
+    public String surname;
+
+    public Student(int id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String toString() {
+        return id + " " + name + " " + surname;
+    }
+}
